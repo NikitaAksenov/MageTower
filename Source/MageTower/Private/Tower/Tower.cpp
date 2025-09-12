71708cbc38ba3206086c7d33b81159f3d9203b96
NikitaAksenov <naksenov96@gmail.com>
2025-09-12 22:38:47 +0300

    Drafted tower actor

---
+++ Source/MageTower/Private/Tower/Tower.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tower/Tower.h"
+
+
+ATower::ATower()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMesh->SetupAttachment(Root);
+}
+
+void ATower::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATower::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/MageTower/Public/Tower/Tower.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Tower.generated.h"
+
+
+UCLASS()
+class MAGETOWER_API ATower : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	ATower();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<USceneComponent> Root;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> StaticMesh;
+};
