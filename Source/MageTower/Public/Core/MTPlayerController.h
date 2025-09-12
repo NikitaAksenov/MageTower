63e3e70d94676bb99ae01772a5844061a41376e3
NikitaAksenov <naksenov96@gmail.com>
2025-09-12 22:10:14 +0300

    Created core classes

---
+++ Source/MageTower/Public/Core/MTPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MTPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGETOWER_API AMTPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
